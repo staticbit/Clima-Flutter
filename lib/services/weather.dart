12387e0abe1f2e6f3cf4fd721ffe807a9960dd05
Karan Singh <karan@singhkaran.com>
2019-08-21 17:26:32 +0530

    Getting City Weather

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,12 +5,21 @@ const apiKey = 'e72ca729af228beabd5d20e3b7749713';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = new Location();
     await location.getCurrentLocation();
 
-    NetworkHelper networkHelper = NetworkHelper(
-        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    var url =
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
 
     var weatherData = await networkHelper.getData();
     return weatherData;
